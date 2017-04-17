6385bed07d7439d82fed7334efb7cf99876c4206
Jefferson762 <jeffersonmoreira522@hotmail.com>
2017-04-17 19:18:25 -0300

    Add files via upload

---
+++ 6yuh67uy.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+
+int n1;
+int n2;
+int num1,num2,num3,num4;
+
+cout << "\nDigite um valor:";
+cin >> n1;
+cout <<"\nDigite outro valor:";
+cin >> n2;
+
+num1=n1/n2;
+num2=n1*n2;
+num3=n1-n2;
+num4=n1+n2;
+
+cout <<"\nDivisao:" << num1;
+cout <<"\nMultiplicacao:" << num2;
+cout <<"\nSubtracao:" << num3;
+cout <<"\nAdicao:" << num4;
+
+system("pause");
+
+
+ return 0;
+
+
+}
